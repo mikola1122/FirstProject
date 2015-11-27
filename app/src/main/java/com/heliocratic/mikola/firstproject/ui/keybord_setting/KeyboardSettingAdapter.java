5b05643f0138fb448b8dfede2c32ea08af251db8
mikola11 <mikola11@i.ua>
2015-11-27 18:40:26 +0200

    ------

--- app/src/main/java/com/heliocratic/mikola/firstproject/ui/keybord_setting/KeyboardSettingAdapter.java
+++ app/src/main/java/com/heliocratic/mikola/firstproject/ui/keybord_setting/KeyboardSettingAdapter.java
@@ -1,7 +1,6 @@
 package com.heliocratic.mikola.firstproject.ui.keybord_setting;
 
 import android.content.Context;
-import android.support.v4.content.res.TypedArrayUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,7 +26,7 @@ public class KeyboardSettingAdapter extends BaseAdapter {
     public KeyboardSettingAdapter(String[] itemTitle, Context context) {
         this.itemTitle = itemTitle;
         this.context = context;
-        layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//        layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
     @Override
@@ -47,14 +46,12 @@ public class KeyboardSettingAdapter extends BaseAdapter {
 
     @Override
     public int getItemViewType(int position) {
-        int[] itemsWithCheckBox = {8, 10, 13, 14, 16};
-        int[] itemsWithSeekBar = {2, 3, 6};
-        if (Arrays.asList(itemsWithCheckBox).contains(position))
+        if (position == 8 || position == 10 || position == 13 || position == 14 || position == 16)
             return TYPE_ITEM_CHECK;
-        else if (Arrays.asList(itemsWithSeekBar).contains(position))
+        else if(position == 2 || position == 3 || position == 6)
             return TYPE_ITEM_SEEKBAR;
-        //other variant(item contains TextView only)
-        return TYPE_ITEM_TEXT;
+         else
+            return TYPE_ITEM_TEXT;
     }
 
     @Override
@@ -68,20 +65,20 @@ public class KeyboardSettingAdapter extends BaseAdapter {
         if (convertView == null){
             switch (getItemViewType(position)) {
                 case TYPE_ITEM_TEXT:
-                    convertView = layoutInflater.inflate(R.layout.item_text_keyboard_setting, parent, false);
+                    convertView = LayoutInflater.from(context).inflate(R.layout.item_text_keyboard_setting, parent, false);
                     TextViewHolder textViewHolder = new TextViewHolder(convertView);
                     textViewHolder.optionsTitle.setText(itemTitle[position]);
                     convertView.setTag(textViewHolder);
                     break;
                 case TYPE_ITEM_CHECK:
-                    convertView = layoutInflater.inflate(R.layout.item_checkbox_keyboard_setting, parent, false);
+                    convertView = LayoutInflater.from(context).inflate(R.layout.item_checkbox_keyboard_setting, parent, false);
                     CheckBoxViewHolder checkBoxViewHolder = new CheckBoxViewHolder(convertView);
                     checkBoxViewHolder.optionsTitle.setText(itemTitle[position]);
                     checkBoxViewHolder.checkBox.setText("on/off");
                     convertView.setTag(checkBoxViewHolder);
                     break;
                 case TYPE_ITEM_SEEKBAR:
-                    convertView = layoutInflater.inflate(R.layout.item_seekbar_keyboard_settings, parent, false);
+                    convertView = LayoutInflater.from(context).inflate(R.layout.item_seekbar_keyboard_settings, parent, false);
                     SeekBarViewHolder seekBarViewHolder = new SeekBarViewHolder(convertView);
                     seekBarViewHolder.optionsTitle.setText(itemTitle[position]);
                     convertView.setTag(seekBarViewHolder);
