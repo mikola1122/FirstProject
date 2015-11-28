23f3855fadf31d8444fb20d44eac27123e901c41
mikola11 <mikola11@i.ua>
2015-11-28 22:40:31 +0200

    ------

--- app/src/main/java/com/heliocratic/mikola/firstproject/ui/keybord_setting/KeyboardSettingAdapter.java
+++ app/src/main/java/com/heliocratic/mikola/firstproject/ui/keybord_setting/KeyboardSettingAdapter.java
@@ -62,84 +62,75 @@ public class KeyboardSettingAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        ViewHolder holder = null;
-
+        TextViewHolder textViewHolder = null;
+        CheckBoxViewHolder checkBoxViewHolder = null;
+        SeekBarViewHolder seekBarViewHolder = null;
         if (convertView == null){
-            holder = new ViewHolder();
             switch (getItemViewType(position)) {
                 case TYPE_ITEM_TEXT:
                     convertView = inflater.inflate(R.layout.item_text_keyboard_setting, parent, false);
-                    holder.optionsTitle = (TextView) convertView.findViewById(R.id.itemTextKeyboardSettingTitle);
-                    holder.optionsTitle.setText(itemTitle.get(position));
-//                    convertView = LayoutInflater.from(context).inflate(R.layout.item_text_keyboard_setting, parent, false);
-//                    TextViewHolder textViewHolder = new TextViewHolder(convertView);
-//                    textViewHolder.optionsTitle.setText(itemTitle.get(position));
-//                    convertView.setTag(textViewHolder);
+                    textViewHolder = new TextViewHolder(convertView);
+                    convertView.setTag(textViewHolder);
                     break;
                 case TYPE_ITEM_CHECK:
                     convertView = inflater.inflate(R.layout.item_checkbox_keyboard_setting, parent, false);
-                    holder.optionsTitle = (TextView) convertView.findViewById(R.id.itemCheckBoxKeyboardSettingTitle);
-                    holder.optionsTitle.setText(itemTitle.get(position));
-                    holder.checkBox = (CheckBox) convertView.findViewById(R.id.CheckBoxKeyboardSetting);
-                    holder.checkBox.setText("on/off");
-//                    convertView = LayoutInflater.from(context).inflate(R.layout.item_checkbox_keyboard_setting, parent, false);
-//                    CheckBoxViewHolder checkBoxViewHolder = new CheckBoxViewHolder(convertView);
-//                    checkBoxViewHolder.optionsTitle.setText(itemTitle.get(position));
-//                    checkBoxViewHolder.checkBox.setText("on/off");
-//                    convertView.setTag(checkBoxViewHolder);
+                    checkBoxViewHolder = new CheckBoxViewHolder(convertView);
+                    convertView.setTag(checkBoxViewHolder);
                     break;
                 case TYPE_ITEM_SEEKBAR:
                     convertView = inflater.inflate(R.layout.item_seekbar_keyboard_settings, parent, false);
-                    holder.optionsTitle = (TextView) convertView.findViewById(R.id.itemSeekBarKeyboardSettingTitle);
-                    holder.optionsTitle.setText(itemTitle.get(position));
-                    holder.seekBar = (SeekBar) convertView.findViewById(R.id.SeekBarKeyboardSetting);
-//                    convertView = LayoutInflater.from(context).inflate(R.layout.item_seekbar_keyboard_settings, parent, false);
-//                    SeekBarViewHolder seekBarViewHolder = new SeekBarViewHolder(convertView);
-//                    seekBarViewHolder.optionsTitle.setText(itemTitle.get(position));
+                    seekBarViewHolder = new SeekBarViewHolder(convertView);
+                    convertView.setTag(seekBarViewHolder);
 //                    seekBarViewHolder.seekBar.setClickable(false);
 //                    seekBarViewHolder.seekBar.setFocusable(false);
 //                    seekBarViewHolder.seekBar.setEnabled(false);
-//                    convertView.setTag(seekBarViewHolder);
                     break;
             }
-            convertView.setTag(holder);
         } else {
-               holder = (ViewHolder) convertView.getTag();
+            switch (getItemViewType(position)){
+                case TYPE_ITEM_TEXT:
+                    textViewHolder = (TextViewHolder) convertView.getTag();
+                    break;
+                case TYPE_ITEM_CHECK:
+                    checkBoxViewHolder = (CheckBoxViewHolder) convertView.getTag();
+                    break;
+                case TYPE_ITEM_SEEKBAR:
+                    seekBarViewHolder = (SeekBarViewHolder) convertView.getTag();
+                    break;
+            }
         }
+        textViewHolder.optionsTitle.setText(itemTitle.get(position));
+        checkBoxViewHolder.optionsTitle.setText(itemTitle.get(position));
+        seekBarViewHolder.optionsTitle.setText(itemTitle.get(position));
+
         return convertView;
     }
 
-    class ViewHolder{
+    class TextViewHolder {
         public TextView optionsTitle;
-        public SeekBar seekBar;
-        public CheckBox checkBox;
+
+        public TextViewHolder(View itemView) {
+            optionsTitle = (TextView) itemView.findViewById(R.id.itemTextKeyboardSettingTitle);
+        }
     }
 
-//    class TextViewHolder {
-//        public TextView optionsTitle;
-//
-//        public TextViewHolder(View itemView) {
-//            optionsTitle = (TextView) itemView.findViewById(R.id.itemTextKeyboardSettingTitle);
-//        }
-//    }
-//
-//    class SeekBarViewHolder {
-//        public TextView optionsTitle;
-//        public SeekBar seekBar;
-//
-//        public SeekBarViewHolder(View itemView) {
-//            optionsTitle = (TextView) itemView.findViewById(R.id.itemSeekBarKeyboardSettingTitle);
-//            seekBar = (SeekBar) itemView.findViewById(R.id.SeekBarKeyboardSetting);
-//        }
-//    }
-//
-//    class CheckBoxViewHolder {
-//        public TextView optionsTitle;
-//        public CheckBox checkBox;
-//
-//        public CheckBoxViewHolder(View itemView) {
-//            optionsTitle = (TextView) itemView.findViewById(R.id.itemCheckBoxKeyboardSettingTitle);
-//            checkBox = (CheckBox) itemView.findViewById(R.id.CheckBoxKeyboardSetting);
-//        }
-//    }
+    class SeekBarViewHolder {
+        public TextView optionsTitle;
+        public SeekBar seekBar;
+
+        public SeekBarViewHolder(View itemView) {
+            optionsTitle = (TextView) itemView.findViewById(R.id.itemSeekBarKeyboardSettingTitle);
+            seekBar = (SeekBar) itemView.findViewById(R.id.SeekBarKeyboardSetting);
+        }
+    }
+
+    class CheckBoxViewHolder {
+        public TextView optionsTitle;
+        public CheckBox checkBox;
+
+        public CheckBoxViewHolder(View itemView) {
+            optionsTitle = (TextView) itemView.findViewById(R.id.itemCheckBoxKeyboardSettingTitle);
+            checkBox = (CheckBox) itemView.findViewById(R.id.CheckBoxKeyboardSetting);
+        }
+    }
 }
--- app/src/main/java/com/heliocratic/mikola/firstproject/ui/keybord_setting/SettingItemWrapper.java
+++
@@ -1,7 +0,0 @@
-package com.heliocratic.mikola.firstproject.ui.keybord_setting;
-
-/**
- * Created by mikola11 on 11/28/2015.
- */
-public class SettingItemWrapper {
-}
